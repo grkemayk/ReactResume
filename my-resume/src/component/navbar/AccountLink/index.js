581086ae39a650e125ec6f5ecf3a2dd4c8891e9c
WISERTECH\gorkem.aykac <grkemaykac1997@hotmail.com>
2022-03-29 15:07:23 +0300

    Deploy Transaction

--- my-resume/src/component/navbar/AccountLink/index.js
+++ my-resume/src/component/navbar/AccountLink/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import style from "./styles.module.css"
+import GithubImage from "./github.png"
+import LinkedInImage from "./linkedin.png"
+
 const AccountLink = () => {
   return (
     <div className={style.accountDiv}>
         <lu className={style.accountList}>
-            <li><a href='https://www.linkedin.com/in/g%C3%B6rkem-ayka%C3%A7-315b11180/?originalSubdomain=tr' target="_blank"><img className={style.accountImage} src="https://cdn-icons.flaticon.com/png/512/2626/premium/2626273.png?token=exp=1648550017~hmac=46850d0467cb93f4984d31bf173891b9"></img></a></li>
-            <li><a href='https://github.com/grkemayk' target="_blank"><img className={style.accountImage} src="https://cdn-icons-png.flaticon.com/512/270/270798.png"></img></a></li>
+            <li><a href='https://www.linkedin.com/in/g%C3%B6rkem-ayka%C3%A7-315b11180/?originalSubdomain=tr' target="_blank"><img className={style.accountImage} src={LinkedInImage}></img></a></li>
+            <li><a href='https://github.com/grkemayk' target="_blank"><img className={style.accountImage} src={GithubImage}></img></a></li>
         </lu>
     </div>
   )
